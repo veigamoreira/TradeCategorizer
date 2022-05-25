8f2d855599332537addda868e20cafbfeb0b9e87
Vagner da Veiga Morreira <vagner.moreira@contractor-mc1.com>
2022-05-25 17:09:21 -0300

    implementation interface method

--- TradeCategoralize/ITrade.cs
+++ TradeCategoralize/ITrade.cs
@@ -10,5 +10,7 @@ namespace TradeCategoralize
         double Value { get; } //indicates the transaction amount in dollars
         string ClientSector { get; } //indicates the client´s sector which can be "Public" or "Private"
         DateTime NextPaymentDate { get; } //indicates when the next payment from the client to the bank is expected
+
+        string ValidateTrade(Trade trade, DateTime dateBase); //operation
     }
 }
