9ba1b1dedcac183be31be9255cf2df24b9aaedc3
Vagner da Veiga Morreira <vagner.moreira@contractor-mc1.com>
2022-05-25 15:24:03 -0300

    implementation

---
+++ TradeCategoralize/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace TradeCategoralize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+            var date = Convert.ToDateTime(Console.ReadLine());
+            var quantity = int.Parse(Console.ReadLine());
+
+            Trade tradeObj  = new Trade();
+            List<Trade> listTrade = new List<Trade>();
+
+           
+            int quantityAux = 0;
+            string[] trade;
+            
+            while (quantityAux < quantity)
+            {
+                trade = Console.ReadLine().Split(" ");
+
+                //checking the array 
+                if (trade.Length == 3)
+                {
+                    tradeObj = new Trade() { Value = Convert.ToDouble(trade[0]), ClientSector = trade[1], NextPaymentDate = DateTime.ParseExact(trade[2], "M/d/yyyy", CultureInfo.InvariantCulture) };
+                    listTrade.Add(tradeObj);
+
+                    quantityAux++;
+                }
+            }
+
+
+            foreach (var item in listTrade)
+            {
+                Console.WriteLine(tradeObj.ValidateTrade(item, date));
+            }
+            Console.ReadKey();
+
+
+            //Question 2 -  proprierty was cread on Itrade Interface, and the only thing to do is made one more if on the ValidateTrade Method
+        }
+    }
+}
---
+++ TradeCategoralize/Trade.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TradeCategoralize
+{
+    public class Trade : ITrade
+    {
+        public double Value { get; set; }
+
+        public string ClientSector { get; set; }
+
+        public DateTime NextPaymentDate { get; set; }
+
+        public bool PEP { get; set; }
+
+
+        public string ValidateTrade(Trade trade, DateTime dateBase)
+        {
+            if (trade.NextPaymentDate.AddDays(30) < dateBase )
+                return "EXPIRED";
+            if (trade.ClientSector.ToUpper() == "PRIVATE" && trade.Value > 1000000)
+                return "HIGHRISK";
+            if (trade.ClientSector.ToUpper() == "PUBLIC" && trade.Value > 1000000)
+                return "MEDIUMRISK";
+
+            return "UNCATEGORIZED";
+        }
+    }
+}
